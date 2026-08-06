c1f7b3a5fbe49e4c62afa5aeefff601174575c60
echo1097 <noahtamsteele@gmail.com>
2026-08-05 22:20:23 -0700

    fixes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "use strict";
 
 const repositoryUrl = "https://github.com/echo1097/routerchat";
+const redirectDelay = 800;
 
-window.location.replace(repositoryUrl);
+window.addEventListener("load", () => {
+  window.setTimeout(() => {
+    window.location.replace(repositoryUrl);
+  }, redirectDelay);
+});
